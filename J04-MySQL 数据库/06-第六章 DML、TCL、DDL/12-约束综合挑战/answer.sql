82fc753aca5a50b4f270e33e1fc639ba5dc12aa0
nfc916 <44668822@163.com>
2022-03-05 14:00:30 +0800

    Update answer.sql

--- J04-MySQL 数据库/06-第六章 DML、TCL、DDL/12-约束综合挑战/answer.sql
+++ J04-MySQL 数据库/06-第六章 DML、TCL、DDL/12-约束综合挑战/answer.sql
@@ -3,7 +3,7 @@ use personnel;
 -- 约束综合挑战
 ALTER TABLE salgrade ADD PRIMARY key LH_PK( `grade`,`losal`,`hisal`);
 ALTER TABLE emp ADD  foreign key fk_dept_id(deptno) references dept(deptno);
-ALTER TABLE emp ADD  add unique  UQ_NAME(ename);
+ALTER TABLE emp ADD  unique  UQ_NAME(ename);
 ALTER TABLE emp ALTER COLUMN comm SET DEFAULT 100;
 ALTER TABLE emp add check(hiredate  < '2022-2-28');
 
