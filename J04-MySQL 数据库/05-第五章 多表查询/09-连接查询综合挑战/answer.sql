0e3c754da7438fd7512d89fa4d91d519dc8c406b
nfc916 <44668822@163.com>
2022-03-10 16:31:08 +0800

    Update answer.sql

--- J04-MySQL 数据库/05-第五章 多表查询/09-连接查询综合挑战/answer.sql
+++ J04-MySQL 数据库/05-第五章 多表查询/09-连接查询综合挑战/answer.sql
@@ -2,6 +2,6 @@ use personnel;
 
 -- 连接查询综合挑战
 select ename '名字',hiredate  '入职时间',dname  '部门名称' from emp , dept where emp.deptno = dept.deptno;
-select ename  '名字',sal  '工资', (case(grade) when "1" then "A"  when "2" then "B"  when "3" then "C"  when "4" then "D"  else "E" end ) as '级别' from emp , salgrade where emp.sal > salgrade.losal and emp.sal<salgrade.hisal;
+select ename  '名字',sal  '工资', (case(grade) when "1" then "A"  when "2" then "B"  when "3" then "C"  when "4" then "D"  else "E" end ) as '级别' from emp , salgrade where emp.sal => salgrade.losal and emp.sal<=salgrade.hisal;
 select b.* from emp a,emp b where b.empno=a.mgr and a.ename='SMITH';
 select * from emp right join dept on emp.deptno=dept.deptno;
