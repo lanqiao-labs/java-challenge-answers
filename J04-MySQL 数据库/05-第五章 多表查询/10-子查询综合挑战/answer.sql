5fbca60d5d59b83d17d8b8200dd758448b3e2072
nfc916 <44668822@163.com>
2022-06-01 16:27:27 +0800

    Update answer.sql

--- J04-MySQL 数据库/05-第五章 多表查询/10-子查询综合挑战/answer.sql
+++ J04-MySQL 数据库/05-第五章 多表查询/10-子查询综合挑战/answer.sql
@@ -3,5 +3,6 @@ use personnel;
 -- 子查询综合挑战
 select ename '名字' from emp where sal = (select max(sal) from emp);
 select dname '部门',(select COUNT(DEPTNO) from emp where DEPTNO = 10) / (select COUNT(*) from emp) as '比例' FROM dept  where DEPTNO = 10;
-select * from EMP where DEPTNO = (select DEPTNO from DEPT where LOC = 'NEW YORK');
+select * from emp where DEPTNO = (select DEPTNO from dept where LOC = 'NEW YORK');
 select * from emp e1 where sal > ( select avg(sal) from emp e2 where e1.deptno = deptno );
+
