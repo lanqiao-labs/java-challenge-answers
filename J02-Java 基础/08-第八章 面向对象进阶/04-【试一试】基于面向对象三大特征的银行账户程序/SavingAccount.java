493bad2cef447dc0be5d15a4bce9558a6961abb4
nfc916 <44668822@163.com>
2024-03-25 17:35:12 +0800

    Update SavingAccount.java

--- J02-Java 基础/08-第八章 面向对象进阶/04-【试一试】基于面向对象三大特征的银行账户程序/SavingAccount.java
+++ J02-Java 基础/08-第八章 面向对象进阶/04-【试一试】基于面向对象三大特征的银行账户程序/SavingAccount.java
@@ -1,19 +1,34 @@
-/**
- * 储蓄账户类 子类
- */
-public class SavingAccount extends Account {
-    private  static  double interest;  //年利率，私有静态
-    public static double getInterest() {  //获取年利率属性
+package bank;
+public class SavingAccount  extends Account{  //子类 储蓄账户
+    private  double interest;  //年利率
+
+ 
+    public void countInterest(){    //计算利息并存入账户
+        double interesty=this.getBalance() * this.interest;
+        this.deposit(interesty);
+    }
+
+
+    public SavingAccount(double interest) {
+        this.interest = interest;
+    }
+
+    public SavingAccount( ) {
+       
+    }
+
+    public SavingAccount(long id, String name, String password, double balance, double interest) {
+        super(id, name, password, balance);
+        this.interest = interest;
+    }
+
+
+    public double getInterest() {
         return interest;
     }
-    public static void setInterest(double interest) {  //设置年利率属性
-        SavingAccount.interest = interest;
-    }
-    public SavingAccount(long id, String name, String password) {
-        super(id, name, password);
-    }
-    public void countInterest(){    //计算利息并存入账户
-        double interesty=this.balance * this.interest;
-        this.deposit(interesty);
+
+
+    public void setInterest(double interest) {
+        this.interest = interest;
     }
 }
