a49feaf174e9d6dbd3317da1fbc6fa67775d4a23
nfc916 <44668822@163.com>
2024-03-31 20:21:57 +0800

    Update FileCopy.java

--- J03-Java 进阶/03-第三章 IO 和 XML/05-实现文件拷贝/FileCopy.java
+++ J03-Java 进阶/03-第三章 IO 和 XML/05-实现文件拷贝/FileCopy.java
@@ -1,3 +1,4 @@
+import java.io.*;
 public class FileCopy {
 
     /**
@@ -6,15 +7,43 @@ public class FileCopy {
      * @param descfile 拷贝后新的文件
      * @return 拷贝是否成功 true - 成功 false - 失败
      */
-    public boolean copyImage(String srcfile, String descfile){
+    public boolean copyImage(String srcfile, String descfile) throws IOException {
+        File sourceFile = new File(srcfile);
+        File targetFile = new File(descfile);
 
-        ### 代码实现 ###
+        if (!sourceFile.exists()) {
+            throw new FileNotFoundException("源图片文件不存在: " + srcfile);
+        }
 
+        // 创建目标目录（如果尚不存在）
+        if (!targetFile.getParentFile().exists()) {
+            targetFile.getParentFile().mkdirs();
+        }
+
+        try (InputStream in = new FileInputStream(sourceFile);
+                OutputStream out = new FileOutputStream(targetFile)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
+        return true;
     }
 
+    // 示例如何使用此方法
     public static void main(String[] args) {
+        String sourceImagePath = "2.jpg";
+        String targetDirectory = "img/image.jpg";
 
-        ### 代码实现 ###
-
+        try {
+            if (new FileCopy().copyImage(sourceImagePath, targetDirectory)) {
+                System.out.println("拷贝成功");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
