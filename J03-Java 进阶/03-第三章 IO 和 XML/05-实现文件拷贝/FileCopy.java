3a427e4f399340ab42fecf920475ff86c2c516fd
nfc916 <44668822@163.com>
2024-03-31 19:20:53 +0800

    Create FileCopy.java

---
+++ J03-Java 进阶/03-第三章 IO 和 XML/05-实现文件拷贝/FileCopy.java
@@ -0,0 +1,20 @@
+public class FileCopy {
+
+    /**
+     * 实现图片拷贝
+     * @param srcfile 被拷贝的文件
+     * @param descfile 拷贝后新的文件
+     * @return 拷贝是否成功 true - 成功 false - 失败
+     */
+    public boolean copyImage(String srcfile, String descfile){
+
+        ### 代码实现 ###
+
+    }
+
+    public static void main(String[] args) {
+
+        ### 代码实现 ###
+
+    }
+}
