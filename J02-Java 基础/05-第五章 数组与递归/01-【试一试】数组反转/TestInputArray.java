a5264e5f031290f242017a7cf9de03c17c8440f1
nfc916 <44668822@163.com>
2024-06-13 17:01:24 +0800

    Create TestInputArray.java

---
+++ J02-Java 基础/05-第五章 数组与递归/01-【试一试】数组反转/TestInputArray.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TestInputArray{
+
+    public static void main(String[] args) {
+        
+        InputArray arr =new InputArray();
+ 
+        arr.showArray(arr.reverseArray(arr.input()));
+
+    }
+
+}
