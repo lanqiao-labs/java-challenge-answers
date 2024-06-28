b70e8cb684646124046c904b9869d77451ed1469
nfc916 <44668822@163.com>
2024-06-28 13:36:51 +0800

    Update Rectangle.java

--- J02-Java 基础/09-第九章 抽象类和接口/02-【试一试】图形接口的设计与实现/Rectangle.java
+++ J02-Java 基础/09-第九章 抽象类和接口/02-【试一试】图形接口的设计与实现/Rectangle.java
@@ -1,53 +1,42 @@
 package org.lanqiao.entity;
 
-public class Triangle implements Figure{
-    private double side1;
-    private double side2;
-    private double side3;
-
-    public Triangle(){}
-
-    public Triangle(double side1,double side2,double side3){
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+public class Rectangle implements Figure{
+    private double width;
+    private double height;
+    
+    public Rectangle(){}
+    
+    public Rectangle(double width,double height){
+        this.width = width;
+        this.height = height;
     }
 
     @Override
     public double getPerimeter() {
         // TODO Auto-generated method stub
-        return side1+side2+side3;
+        return width*2+height*2;
     }
 
     @Override
     public double getArea() {
         // TODO Auto-generated method stub
-        double p = (side1+side2+side3)/2;
-        return Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+        return width*height;
     }
 
-    public double getSide1() {
-        return side1;
+    public double getWidth() {
+        return width;
     }
 
-    public void setSide1(double side1) {
-        this.side1 = side1;
+    public void setWidth(double width) {
+        this.width = width;
     }
 
-    public double getSide2() {
-        return side2;
+    public double getHeight() {
+        return height;
     }
 
-    public void setSide2(double side2) {
-        this.side2 = side2;
+    public void setHeight(double height) {
+        this.height = height;
     }
-
-    public double getSide3() {
-        return side3;
-    }
-
-    public void setSide3(double side3) {
-        this.side3 = side3;
-    }
-
+    
 }
