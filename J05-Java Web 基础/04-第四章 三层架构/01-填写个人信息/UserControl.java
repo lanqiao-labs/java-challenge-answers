8a695a63b29e35b6749a9563ffc98dd325852da9
nfc916 <44668822@163.com>
2025-01-06 10:06:05 +0800

    Update UserControl.java

--- J05-Java Web 基础/04-第四章 三层架构/01-填写个人信息/UserControl.java
+++ J05-Java Web 基础/04-第四章 三层架构/01-填写个人信息/UserControl.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/UserControl")
+@WebServlet("/addUserInfo")
 public class UserControl extends HttpServlet {
     /**
      *
